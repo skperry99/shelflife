8b12b2b94821718598fdd8a732fb04c492df8ee5
skperry99 <sperry939@gmail.com>
2025-11-24 20:11:53 -0600

    Before flattening backend folder structure

---
+++ backend/shelflife-backend/src/main/java/org/saper/shelflife/ShelflifeBackendApplication.java
@@ -0,0 +1,13 @@
+package org.saper.shelflife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelflifeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelflifeBackendApplication.class, args);
+    }
+
+}
