577d808b299bea965638b6d73af690923787b67d
skperry99 <sperry939@gmail.com>
2025-11-25 20:48:27 -0600

    Added routes to App.jsx

--- shelflife-frontend/src/App.jsx
+++ shelflife-frontend/src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
 
-function App() {
+import LibraryPage from "./pages/LibraryPage.jsx";
+import WorkDetailPage from "./pages/WorkDetailPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+// Later: import ReviewsPage, StatsPage, ProfilePage, etc.
 
+export default function App() {
   return (
-    <>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<LibraryPage />} />
+      <Route path="/works/:workId" element={<WorkDetailPage />} />
+      {/* <Route path="/reviews" element={<ReviewsPage />} /> */}
+      {/* <Route path="/stats" element={<StatsPage />} /> */}
+      {/* <Route path="/profile" element={<ProfilePage />} /> */}
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 }
-
-export default App
