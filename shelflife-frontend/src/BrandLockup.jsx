40ba1e2412d581034bcdbeb4182974e3493a7767
skperry99 <sperry939@gmail.com>
2025-11-25 21:03:40 -0600

    Added BrandLockup.jsx

---
+++ shelflife-frontend/src/BrandLockup.jsx
@@ -0,0 +1,9 @@
+export default function BrandLockup() {
+  return (
+    <img
+      src="/shelflife-wordmark.svg"
+      alt="ShelfLife – Reading & media journal"
+      style={{ height: "48px" }}
+    />
+  );
+}
